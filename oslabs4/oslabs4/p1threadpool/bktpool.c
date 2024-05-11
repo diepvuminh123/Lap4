8a44c4e678d6d5646d3da3821d7fe5a886b079a3
Minhdev2004 <chinhtpc123@gmail.com>
2024-05-11 19:44:30 +0700

    test

---
+++ oslabs4/oslabs4/p1threadpool/bktpool.c
@@ -0,0 +1,12 @@
+// Group members:
+//   Tran Tuan Kiet - 2252392
+//   Ho Trong Nghia - 2252526
+//   Nguyen Nghiem  - 2252521
+//   Dinh Hai Nam   - 2252498
+
+#include "bktpool.h"
+
+int bktpool_init()
+{
+   return bkwrk_create_worker();
+} 
